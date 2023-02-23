a0311e1472d48e184c25f1d165a9168cf18bcb77
gustavwiese <gusse12345@gmail.com>
2023-02-23 13:33:52 +0100

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,21 @@ function decreaseNumber() {
 }
 
 function displayNumber() {
+  let message = "";
+  if (number > 10) {
+    message = "Tallet er større end 10";
+    console.log("Tallet er større end 10");
+  } else if (number == 10) {
+    message = "Tallet er 10";
+    console.log("Tallet er 10")
+  } else {
+    message = "Tallet er mindre end 10";
+    console.log("Tallet er 10 eller mindre");
+  }
   document.querySelector("#number").textContent = number;
-  let message = `Tallet er nu ${number}`;
   document.querySelector("#message").textContent = message;
 }
+
+console.log(number);
+
+console.log(23+23);
